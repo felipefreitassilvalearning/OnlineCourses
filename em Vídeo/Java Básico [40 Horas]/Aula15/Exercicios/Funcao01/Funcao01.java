25aa47fafe3eec49ce33c8c0cf0a1065f4c5f813
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-07-04 06:50:12 -0300

    POO com classe Fatorial

---
+++ em Vídeo/Java Básico [40 Horas]/Aula15/Exercicios/Funcao01/Funcao01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package Exercicios.Funcao01;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Funcao01 {
+    public static void main(String[] args) {
+        Fatorial f = new Fatorial();
+        f.setValor(5);
+        System.out.print(f.getFormula());
+        System.out.println(f.getFatorial());
+    }
+}
