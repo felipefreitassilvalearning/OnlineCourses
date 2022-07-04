6dbe0ba36eb33181cd225e0be70e60ac497a8073
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-07-04 01:04:19 -0300

    Update OperadoresAritmeticos.java
    Math class studies

--- em Vídeo/Java Básico [40 Horas]/Aula07/OperadoresAritmeticos.java
+++ em Vídeo/Java Básico [40 Horas]/Aula07/OperadoresAritmeticos.java
@@ -9,5 +9,29 @@ public class OperadoresAritmeticos {
         int valor = 4 + --numero;
         System.out.println(valor);
         System.out.println(numero);
+
+        int x = 4;
+        x += 2; // x = x + 2
+        x *= 2;
+        System.out.println(x);
+
+        System.out.println(Math.PI);
+        System.out.println(Math.pow(2, 12));
+        System.out.println(Math.sqrt(256));
+        System.out.println(Math.cbrt(4096));
+
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.floor(3.9));
+        System.out.println(Math.ceil(4.2));
+        System.out.println(Math.round(4.499999999999999));
+        System.out.println(Math.round(4.4999999999999999));
+
+        // Get a random number
+        System.out.println(Math.random());
+
+        // Get a random number between a and b
+        int a = 1;
+        int b = 100;
+        System.out.println(a + Math.random() * (b - a));
     }
 }
