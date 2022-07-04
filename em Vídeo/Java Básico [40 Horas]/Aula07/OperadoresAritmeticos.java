a7b1c54bddf99115c8396edcdf1d4a66c6376264
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-07-04 00:49:34 -0300

    Create OperadoresAritmeticos.java
    Base division

---
+++ em Vídeo/Java Básico [40 Horas]/Aula07/OperadoresAritmeticos.java
@@ -0,0 +1,8 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2) / 2;
+        System.out.println("A média é igual a: " + m);
+    }
+}
