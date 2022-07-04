338730869383150ac8692f5de08e8868a648590c
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-07-04 00:52:40 -0300

    Update OperadoresAritmeticos.java
    Pre incremento e pos decremento testing

--- em Vídeo/Java Básico [40 Horas]/Aula07/OperadoresAritmeticos.java
+++ em Vídeo/Java Básico [40 Horas]/Aula07/OperadoresAritmeticos.java
@@ -4,5 +4,10 @@ public class OperadoresAritmeticos {
         int n2 = 5;
         float m = (n1 + n2) / 2;
         System.out.println("A média é igual a: " + m);
+
+        int numero = 10;
+        int valor = 4 + --numero;
+        System.out.println(valor);
+        System.out.println(numero);
     }
 }
