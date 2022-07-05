d1e934d2032967c9fb7a8675f5ce7ea0ada05fd4
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-07-05 01:05:54 -0300

    Create igualidentico.php
    Diferenca entre igualdade e "identicidade"

---
+++ em Vídeo/PHP Básico [40 Horas]/Aula07/igualidentico.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+       $a = 3;
+       $b = "3";
+       $r = ($a === $b)?"SIM":"NAO";
+       echo "As variaveis A e B sao identicas? $r";
+    ?>
+</div>
+</body>
+</html>
