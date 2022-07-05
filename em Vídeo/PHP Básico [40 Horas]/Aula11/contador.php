bfb0d766e444eed905e1921525abd0d0bed18682
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-07-05 03:50:46 -0300

    Aula 11 - Laços de repetição
    E variáveis de variáveis
    TO DO: Exerciocio final de aula contador dinamico

---
+++ em Vídeo/PHP Básico [40 Horas]/Aula11/contador.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <link rel="stylesheet" href="../MODELO/_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+    <body>
+        <div>
+            <?php
+                $c = 1;
+                while ($c <= 15) {
+                    echo $c."<br>";
+                    $c++;
+                }
+            ?>
+        </div>
+    </body>
+</html>
