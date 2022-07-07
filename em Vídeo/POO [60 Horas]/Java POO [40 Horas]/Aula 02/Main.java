9f6718f893b7d889e00ba5085951c67efd73d470
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-07-07 03:41:31 -0300

    Update Main.java
    Adjusting comments

--- em Vídeo/POO [60 Horas]/Java POO [40 Horas]/Aula 02/Main.java
+++ em Vídeo/POO [60 Horas]/Java POO [40 Horas]/Aula 02/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
         c1.cor = "Azul";
-        c1.ponta = 0.5f;
+        // c1.ponta = 0.5f;
         c1.tampar();
-        // c1.status();
+        c1.status();
         c1.rabiscar();
 
         Caneta c2 = new Caneta();
