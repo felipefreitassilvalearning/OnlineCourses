435dfeefdacfef3dbbae32f0b554cc1f6296d4bb
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-07-08 21:10:30 -0300

    Primeiro Exercicio
    Criadas 2 contas de banco e realizadas operações básicas

---
+++ em Vídeo/POO [60 Horas]/Java POO [40 Horas]/Aula 05/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        ContaBanco p1 = new ContaBanco();
+        p1.setNumConta(1111);
+        p1.setDono("Jubileu");
+        p1.abrirConta("Cc");
+
+        ContaBanco p2 = new ContaBanco();
+        p2.setNumConta(2222);
+        p2.setDono("Creuza");
+        p2.abrirConta("Cp");
+
+        p1.depositar(100);
+        p2.depositar(500);
+        p2.sacar(1000);
+
+        p1.fecharConta();
+
+        System.out.println(p1);
+        System.out.println(p2);
+    }
+}
