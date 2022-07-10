c69cae1284bca3d7401758998e39900bf9d18776
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-07-09 22:52:11 -0300

    Exercicio pratico de Herança
    Superclasse pessoa é mãe de professores e alunos, alunos por sua vez são extendidos por Bolsistas e Tecnicos

---
+++ em Vídeo/POO [60 Horas]/Java POO [40 Horas]/Aula 11/Bolsista.java
@@ -0,0 +1,20 @@
+public class Bolsista extends Aluno {
+    private float bolsa;
+
+    public float getBolsa() {
+        return bolsa;
+    }
+
+    public void setBolsa(float bolsa) {
+        this.bolsa = bolsa;
+    }
+
+    public void renovarBolsa() {
+        System.out.println("Renovando bolsa de " + this.nome);
+    }
+
+    @Override
+    public void pagarMensalidade() {
+        System.out.println(this.nome + " é bolsista! Pagamento facilitado");
+    }
+}
