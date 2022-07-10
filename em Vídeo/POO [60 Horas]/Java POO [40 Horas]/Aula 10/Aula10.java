34c296820e9d00f45c50f9cdcd87b21d258d8fa6
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-07-09 22:50:50 -0300

    Correção de Nome, conflito de Classes
    Aula 11 também utiliza professor, extenção do commit anterior

--- em Vídeo/POO [60 Horas]/Java POO [40 Horas]/Aula 10/Aula10.java
+++ em Vídeo/POO [60 Horas]/Java POO [40 Horas]/Aula 10/Aula10.java
@@ -2,7 +2,7 @@ public class Aula10 {
     public static void main(String[] args) {
         Pessoa10 p1 = new Pessoa10();
         Aluno10 p2 = new Aluno10();
-        Professor p3 = new Professor();
+        Professor10 p3 = new Professor10();
         Funcionario p4 = new Funcionario();
 
         p1.setNome("Pedro");
--- em Vídeo/POO [60 Horas]/Java POO [40 Horas]/Aula 10/Professor10.java
+++ em Vídeo/POO [60 Horas]/Java POO [40 Horas]/Aula 10/Professor10.java
@@ -1,4 +1,4 @@
-public class Professor extends Pessoa10 {
+public class Professor10 extends Pessoa10 {
     private String especialidade;
     private float salario;
 
