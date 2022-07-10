69f3fc04bc69008a98bdf556bda68d6ef75b4e58
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-07-09 21:38:39 -0300

    Exercicio Pratico Leitura
    Pessoa Le livro que implementa uma interface Publicacoa

---
+++ em Vídeo/POO [60 Horas]/Java POO [40 Horas]/Aula 09/Aula09.java
@@ -0,0 +1,19 @@
+public class Aula09 {
+    public static void main(String[] args) {
+        Pessoa[] p = new Pessoa[2];
+        Livro[] l = new Livro[3];
+
+        p[0] = new Pessoa("Pedro", 22, "M");
+        p[1] = new Pessoa("Maria", 25, "F");
+
+        l[0] = new Livro("Aprendendo Java", "José da Silva", 300, p[0]);
+        l[1] = new Livro("POO para Iniciantes", "Pedro Paulo", 500, p[1]);
+        l[2] = new Livro("Java Avançado", "Maria Candido", 800, p[0]);
+
+        l[0].abrir();
+        l[0].folhear(200);
+        l[0].avancarPag();
+        System.out.println(l[0]);
+        System.out.println(l[1]);
+    }
+}
---
+++ em Vídeo/POO [60 Horas]/Java POO [40 Horas]/Aula 09/Livro.java
@@ -0,0 +1,104 @@
+public class Livro implements Publicacao {
+    private String titulo;
+    private String autor;
+    private int totPaginas;
+    private int pagAtual;
+    private boolean aberto;
+    private Pessoa leitor;
+
+    public Livro(String titulo, String autor, int totPaginas, Pessoa leitor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.totPaginas = totPaginas;
+        setPagAtual(0);
+        setAberto(false);
+        this.leitor = leitor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getTotPaginas() {
+        return totPaginas;
+    }
+
+    public void setTotPaginas(int totPaginas) {
+        this.totPaginas = totPaginas;
+    }
+
+    public int getPagAtual() {
+        return pagAtual;
+    }
+
+    public void setPagAtual(int pagAtual) {
+        this.pagAtual = pagAtual;
+    }
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public Pessoa getLeitor() {
+        return leitor;
+    }
+
+    public void setLeitor(Pessoa leitor) {
+        this.leitor = leitor;
+    }
+
+    public String toString() {
+        return (""
+                + "Título: " + this.getTitulo()
+                + "\nAutor: " + this.getAutor()
+                + "\nPágina Atual: " + this.getPagAtual()
+                + "\nTotal de Páginas: " + this.getTotPaginas()
+                + "\nAberto: " + this.isAberto()
+                + "\nLeitor: \n" + this.getLeitor());
+    }
+
+    @Override
+    public void abrir() {
+        this.setAberto(true);
+    }
+
+    @Override
+    public void fechar() {
+        this.setAberto(false);
+    }
+
+    @Override
+    public void folhear(int pagina) {
+        if (pagina <= this.getTotPaginas()) {
+            this.setPagAtual(pagina);
+        } else {
+            this.setPagAtual(this.getTotPaginas());
+        }
+    }
+
+    @Override
+    public void avancarPag() {
+        this.setPagAtual(this.getPagAtual() + 1);
+    }
+
+    @Override
+    public void voltarPag() {
+        this.setPagAtual(this.getPagAtual() - 1);
+    }
+}
