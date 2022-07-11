04c1de612be4f5afd64857fd80b990fd767b80e5
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-07-11 02:01:42 -0300

    Projeto Final - Parte 1
    Implementação de 3 classes e uma interface, conforme diagrama

---
+++ em Vídeo/POO [60 Horas]/Java POO [40 Horas]/Aula 14/AcoesVideo.java
@@ -0,0 +1,7 @@
+public interface AcoesVideo {
+    public void play();
+
+    public void pause();
+
+    public void like();
+}
---
+++ em Vídeo/POO [60 Horas]/Java POO [40 Horas]/Aula 14/Pessoa.java
@@ -0,0 +1,52 @@
+public abstract class Pessoa {
+    protected String nome;
+    protected int idade;
+    protected String sexo;
+    protected float experiencia;
+
+    protected Pessoa(String nome, int idade, String sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public float getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(float experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public String toString() {
+        return (""
+                + "Nome: " + nome
+                + "\nIdade: " + idade
+                + "\nSexo: " + sexo);
+    }
+
+}
